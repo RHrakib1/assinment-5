477ec1e8f5a5e4a1e2fc043486d9672cb3629327
RHrakib1 <rakibhasanmd457@gmail.com>
2024-02-19 18:14:43 +0600

    add first function

---
+++ function.js
@@ -0,0 +1,10 @@
+function nextBtn() {
+    // hidden add element
+    const getHide = document.getElementById('first-section');
+    getHide.classList.add('hidden');
+
+    // hidden remove elment
+    const removeHide = document.getElementById('second-Section');
+    removeHide.classList.remove('hidden')
+
+}
